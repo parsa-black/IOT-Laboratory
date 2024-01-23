098888d6ca30fa112b1313216720641e4afecaee
parsa <parsagorgzan1225@gmail.com>
2024-01-24 00:49:14 +0330

    Connect to MQTT to Thingsboard

--- Temp & Humidity/Esp32/main.py
+++ Temp & Humidity/Esp32/main.py
@@ -1,20 +1,72 @@
-from machine import Pin
+from umqtt import MQTTClient
+from machine import Pin, reset, Timer
 from time import sleep
+import network
+import ujson
 import dht
+import random
 
+
+# wifi conenction credentials
+SSID = "wifi ssid"
+PASSWORD = "wifi pass"
+
+# MQTT Server Parameters
+MQTT_CLIENT_ID = "iot-lab"
+MQTT_BROKER    = "http://iot.scu.ac.ir:8080/"
+MQTT_USER      = ""
+MQTT_PASSWORD  = ""
+MQTT_TOPIC     = "v1/devices/Parsa/temp&humidity"
+
+wlan_sta = network.WLAN(network.STA_IF)
+wlan_sta.active(True)
+wlan_sta.connect(SSID, PASSWORD)
+
+print("trying to connect to wifi")
+while not wlan_sta.isconnected():
+    pass
+
+# Pin Setup
 sensor = dht.DHT11(Pin(14))
 sleep(1)
 
+# setup the timer
+timer = Timer(0)
+
+def sub_callback(topic, msg):
+  print((topic, msg))
+  
+    
+def connect_and_subscribe():
+  client = MQTTClient(MQTT_CLIENT_ID, MQTT_BROKER,user=MQTT_USER,password=MQTT_PASSWORD)
+  client.set_callback(sub_callback)
+  client.connect()
+  client.subscribe(MQTT_TOPIC)
+  print('Connected to %s MQTT broker, subscribed to %s topic' % (MQTT_BROKER, MQTT_TOPIC))
+  return client
+
+def restart_and_reconnect():
+  print('Failed to connect to MQTT broker. Reconnecting...')
+  sleep(10)
+  reset()
+  
+try:
+  client = connect_and_subscribe()
+except OSError as e:
+  restart_and_reconnect()
+
+def get_states(timer):
+  dht11.measure()
+  temp = sensor.temperature()
+  humidity = sensor.humidity()
+  client.publish(MQTT_TOPIC,ujson.dumps({"temperature":temp,"humidity":humidity}))
+  
+
+timer.init(period=3000, callback=get_states)
+
 while True:
   try:
-    sleep(3)
-    sensor.measure()
-    temp = sensor.temperature()
-    hum = sensor.humidity()
-    temp_f = temp * (9/5) + 32.0
-    print('-' * 25)
-    print('Temperature: %3.1f C' %temp)
-    print('Humidity: %3.1f %%' %hum)
+    client.check_msg()
   except OSError as e:
-    print('Failed to read DHT sensor.')
+    restart_and_reconnect()
     
