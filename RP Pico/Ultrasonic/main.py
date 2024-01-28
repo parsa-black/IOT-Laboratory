d9158fbf9dc2e35ac757dbc6d2ac27dbde4cd84c
parsa <parsagorgzan1225@gmail.com>
2024-01-28 13:50:06 +0330

    HY-SRF05

--- RP Pico/Ultrasonic/main.py
+++ RP Pico/Ultrasonic/main.py
@@ -3,7 +3,6 @@ import utime
 
 trigger = Pin(3, Pin.OUT)
 echo = Pin(2, Pin.IN)
-led = Pin(25, Pin.OUT)
 
 def ultrasonic():
     timepassed = 0
@@ -17,13 +16,11 @@ def ultrasonic():
     while echo.value() == 1:
         signalon = utime.ticks_us()
     timepassed = signalon - signalloff
-    return timepassed
-
-while True:
-    led.toggle()
-    measured_time = ultrasonic()
-    distance_cm = (measured_time * 0.0343) / 2
+    distance_cm = (timepassed * 0.0343) / 2
     distance_cm = round(distance_cm,2)
     print(f"Distance: {distance_cm}")
+
+while True:
+    ultrasonic()
     utime.sleep(1)
     
