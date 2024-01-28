f692e56081380503a9a6f8256e2b90ea43f86cb3
parsa <parsagorgzan1225@gmail.com>
2024-01-28 20:25:43 +0330

    HY-SRF05

--- RP Pico/Ultrasonic/main.py
+++ RP Pico/Ultrasonic/main.py
@@ -1,27 +1,88 @@
 from machine import Pin
+from umqtt.simple import MQTTClient
 import utime
+import time
+import network
+import json
 
-trigger = Pin(3, Pin.OUT)
-echo = Pin(2, Pin.IN)
+# Objects
+TRIGGER = Pin(3, Pin.OUT)
+ECHO = Pin(2, Pin.IN)
+LED = Pin("LED", Pin.OUT)
+GREEN_Led = Pin(18, Pin.OUT)
+
+# WiFi
+SSID = "PARSA"
+SSID_PASSWORD = "44527481"
+
+# Connect To WiFi
+def do_connect():
+    time.sleep(1)
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(SSID, SSID_PASSWORD)
+        while not sta_if.isconnected():
+            print("Attempting to connect....")
+            time.sleep(1)
+    print('Connected! Network config:', sta_if.ifconfig())
+    GREEN_Led.value(1)
+    time.sleep(2)
+
+# Check Connection
+print("Connecting to your wifi...")
+do_connect()
+GREEN_Led.value(0)
+
+# Global variables and constants:
+username="Parsa"
+broker=  "iot.scu.ac.ir"
+topic = "v1/devices/me/telemetry"
+Mqtt_CLIENT_ID = "PicoSonic"
+PASSWORD="scu99ce"
+# MQTT
+client = MQTTClient(client_id=Mqtt_CLIENT_ID, server=broker, port=1883,
+                    user=username, password=PASSWORD, keepalive=10000) #Confiuracion del Cliente MQTT
+client.connect()
+UPDATE_TIME_INTERVAL = 5000 # in ms unit
+last_update = time.ticks_ms()
+# **************************************#
+data = dict()
+#***************************************#
 
 def ultrasonic():
     timepassed = 0
-    trigger.low()
+    TRIGGER.low()
     utime.sleep_us(2)
-    trigger.high()
+    TRIGGER.high()
     utime.sleep_us(5)
-    trigger.low()
-    while echo.value() == 0:
+    TRIGGER.low()
+    while ECHO.value() == 0:
         signalloff = utime.ticks_us()
-    while echo.value() == 1:
+    while ECHO.value() == 1:
         signalon = utime.ticks_us()
     timepassed = signalon - signalloff
     distance_cm = (timepassed * 0.0343) / 2
     distance_cm = round(distance_cm,2)
     return distance_cm
 
+
 while True:
-    D = ultrasonic()
-    print(D)
-    utime.sleep(3)
+    if  time.ticks_ms() - last_update >= UPDATE_TIME_INTERVAL:
+         try:
+            Distance = ultrasonic()
+            data["Distance"] = Distance
+            dataJson = json.dumps(data)
+        
+            print('connection finished')
+            LED.on()
+            client.publish(topic, dataJson)
+            print(f'Distance: {Distance}')
+            print("Data_Published")
+        
+            last_update = time.ticks_ms()
+            
+         except OSError as e:
+            print('Failed to read SRF05 sensor.')   
     
