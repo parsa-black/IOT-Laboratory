4c90c892821a4fb9258b6fe2af61c6549f8ce2e3
parsa <parsagorgzan1225@gmail.com>
2024-01-31 04:19:57 +0330

    Servo-Motor

---
+++ RP Pico/GS90/Servo.py
@@ -0,0 +1,20 @@
+from machine import Pin, PWM
+import utime
+
+MID = 1500000
+MIN = 1000000
+MAX = 2000000
+
+led = Pin(25,Pin.OUT)
+pwm = PWM(Pin(15))
+
+pwm.freq(50)
+pwm.duty_ns(MID)
+
+while True:
+    pwm.duty_ns(MIN)
+    utime.sleep(1)
+    pwm.duty_ns(MID)
+    utime.sleep(1)
+    pwm.duty_ns(MAX)
+    utime.sleep(1)
