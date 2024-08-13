586ed8878b3222b050c2d2e6f997c5fa392de83a
parsa <parsagorgzan1225@gmail.com>
2024-08-13 18:41:50 +0330

    DHT11 Arduino

---
+++ ESP32/Temp & Humidity/Arduino/DHT11.ino
@@ -0,0 +1,48 @@
+#include <SPI.h>
+#include <DHT.h>
+#include <LoRa.h>
+
+//define the pins used by the DHT11 sensor
+#define DHTPIN 13
+#define DHTTYPE DHT11  
+
+//define the pins used by the transceiver module
+#define ss 5
+#define rst 14
+#define dio0 2
+
+int counter = 0;
+
+//initialize DHT11 sensor
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
+
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.println(F("°C "));
+
+}
